62216541a40d0e97af1006d086f8d1263d23cf71
David Andelin <iandelin@outlook.com>
2016-02-10 17:27:03 -0700

    finished assignment

--- callbackPractice-master/callbackPractice.js
+++ callbackPractice-master/callbackPractice.js
@@ -1,78 +1,60 @@
 /* In this repo your job is to write functions to make each function call work properly.
-Below is a sample problem 
+Below is a sample problem
 
   //code here for sayHi
 
-   sayHi('Hi Katie', function(thingToSay){
-      alert(thingToSay);
-   });
-   
+  sayHi('Hi Katie', function(thingToSay) {
+    alert(thingToSay);
+  });
 
-and what you should write is the sayHi function that makes the code above work, 
-    
-    
-   var sayHi = function(str, cb){
+  and what you should write is the sayHi
+  function that makes the code above work,
+
+  var sayHi = function(str, cb) {
     cb(str);
-   }
+  }
 
-   sayHi('Hi Katie', function(thingToSay){
-      alert(thingToSay); //should alert ('Hi Katie')'
-   });
-    
-    
+  sayHi('Hi Katie', function(thingToSay) {
+    alert(thingToSay); //should alert ('Hi Katie')'
+  });
+
 */
 
-
-
-  //Code Here for first
-  
+var first = function(str, cb) {
+ cb(str[0]);
+};
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
 first(names, function(firstName){
   console.log('The first name in names is ' + firstName)
 });
 
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
-
-
-
-  //Code Here for last
+var last = function(str, cb) {
+  cb(str[str.length-1]);
+};
 
 last(names, function(lastName){
   console.log('The last name in names is ' + lastName);
 });
 
-
-
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
-
-
-
-
-
-  //Code Here for multiply
+var multiply = function(x, y, cb) {
+  cb(x * y);
+};
 
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
-})
-
-
-
-
+});
 
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
-
-
-
-
-  //Code Here for contains
+var contains = function(arr, a, cb) {
+  cb(arr.indexOf(a) !== -1);
+};
 
 contains(names, 'Colt', function(result){
   if(result === true){
@@ -82,47 +64,34 @@ contains(names, 'Colt', function(result){
   }
 });
 
-
-
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
-
-
-
-    //Code Here for uniq
+var uniq = function(arr, cb) {
+  cb(arr.reduce(function(prev, cur) {if (prev.indexOf(cur) == -1) prev.push(cur);return prev;},[]));
+};
 
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
 });
 
-
-
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
-
-
-
-    //Code Here for each
+var each = function(arr, cb) {
+  arr.map(cb);
+};
 
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
 });
 
-
-
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
-
-
-
-
- //code here for getUserById
+var getUserById = function(arr, x, cb) {
+  var tmp= arr.filter(function(value){
+    return (value.id == x) ? true: false;
+  })[0];
+  cb(tmp);
+};
 
 var users = [
   {
@@ -142,9 +111,9 @@ var users = [
     email: 'ryan@gmail.com',
     name: 'Ryan',
     address: '192 East 32 North'
-  },
+  }
 ];
 
 getUserById(users, '16t', function(user){
-  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address);
 });
